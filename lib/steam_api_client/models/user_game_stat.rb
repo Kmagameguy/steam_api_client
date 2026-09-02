c629af4abcc9baed0d497d0d268685383c168836
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-09-01 21:44:47 -0400

    Fix User Stats Fetching & Include steam_id

--- lib/steam_api_client/models/user_game_stat.rb
+++ lib/steam_api_client/models/user_game_stat.rb
@@ -3,11 +3,12 @@
 module SteamApiClient
   module Models
     class UserGameStat
-      attr_reader :name, :value
+      attr_reader :steam_id, :name, :value
 
       def initialize(raw_attributes = {})
-        @name  = raw_attributes["_key_name"]
-        @value = raw_attributes["value"]
+        @steam_id = raw_attributes["steam_id"].to_i
+        @name     = raw_attributes["_key_name"]
+        @value    = raw_attributes["value"]
       end
     end
   end
